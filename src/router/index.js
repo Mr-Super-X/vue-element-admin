9f5c9cdc3494d4bd58548f590106225afad0a948
chenjianping <chenjianping3@lvshou.com>
2019-05-23 10:20:35 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from '@/store/index' // 引入vuex
 
-// 静态模块
-import Login from '@/views/login/login'
+// 基础布局组件
 import layout from '@/views/layout/layout'
 
 // 动态路由模块，此处只提供开发环境使用，生产环境需要注释掉路由的权限表
@@ -12,6 +11,8 @@ import dynamic_list from './dynamic_list'
 Vue.use(Router)
 
 const router = new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({y: 0}),
   routes: [
     {
       path: '/',
@@ -20,13 +21,25 @@ const router = new Router({
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: () => import('@/views/login/login')
+    },
+    {
+      path: '/401',
+      name: 'Page401',
+      hidden: true,
+      component: () => import('@/views/error-page/401')
+    },
+    {
+      path: '/404',
+      name: 'Page404',
+      hidden: true,
+      component: () => import('@/views/error-page/404')
     },
     {
       path: '/',
       name: 'Welcome',
       component: layout,
-      hidden: true,
+      hidden: true, //判断路由入口是否可见
       meta: {
         keepAlive: false,
         title: '欢迎'
@@ -39,46 +52,6 @@ const router = new Router({
         meta: {
           keepAlive: false,
           title: "欢迎登录"
-        }
-      }]
-    },
-    {
-      path: '/',
-      name: '404',
-      component: layout,
-      hidden: true,
-      meta: {
-        keepAlive: false,
-        title: '404'
-      },
-      children: [{
-        path: '/404',
-        name: 'error_404',
-        leaf: false,
-        component: () => import('@/views/404/error_404'),
-        meta: {
-          keepAlive: false,
-          title: "404错误"
-        }
-      }]
-    },
-    {
-      path: '/',
-      name: '500',
-      component: layout,
-      hidden: true,
-      meta: {
-        keepAlive: false,
-        title: '500'
-      },
-      children: [{
-        path: '/500',
-        name: 'error_500',
-        leaf: false,
-        component: () => import('@/views/500/error_500'),
-        meta: {
-          keepAlive: false,
-          title: "500错误"
         }
       }]
     },
@@ -102,6 +75,7 @@ const router = new Router({
         }
       }]
     },
+    {path: '*', redirect: '/404', hidden: true},
     //开发新功能，开启本地路由表，并注释掉vuex中对路由表的处理代码
     ...dynamic_list
   ]
@@ -111,7 +85,7 @@ const router = new Router({
 //路由导航拦截，全局钩子，可以在这里做校验（登陆和权限）
 router.beforeEach((to, from, next) => {
   // 不需要登录权限的路由数组
-  const nextRoute = ['login', 'error_404', 'error_500', 'welcome', 'help'];
+  const nextRoute = ['login', 'Page401', 'Page404'];
   // 获取vuex中保存的token信息
   const token = store.getters.token;
 
@@ -123,7 +97,7 @@ router.beforeEach((to, from, next) => {
     // 查看登录权限
     if (!token) {
       next({name: 'login'});
-    }else {
+    } else {
       next();
     }
   }
